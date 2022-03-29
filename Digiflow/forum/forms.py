834334bbc285cc02b7f173e5b9136649077a7e8c
Mayel <pelle.mayel@gmail.com>
2022-03-29 23:43:40 +0200

    add new question

--- Digiflow/forum/forms.py
+++ Digiflow/forum/forms.py
@@ -57,3 +57,13 @@ class AnswerForm(forms.ModelForm):
         super(AnswerForm, self).__init__(*args, **kwargs)
         self.fields['body'] = forms.CharField(max_length=1500, required=True,
                                               widget=forms.Textarea(attrs={'class': 'form-control'}))
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['body']
+
+    def __init__(self, *args, **kwargs):
+        super(AnswerForm, self).__init__(*args, **kwargs)
+        self.fields['body'] = forms.CharField(max_length=1500, required=True,
+                                              widget=forms.Textarea(attrs={'class': 'form-control'}))
